6fcb0d59294d00e6dee93543a9bd298cfd1a13fc
hariharanrdev <hariharanrdev@gmail.com>
2025-11-21 10:10:13 +0530

    feat: add Border_Properties HTML and CSS files with styling for boxes

--- JavaScript/tracing.js
+++ JavaScript/tracing.js
@@ -1,3 +1,6 @@
+/*
+// ! Example 1
+
 console.log("start")
 
 var a = 10;
@@ -14,5 +17,23 @@ console.log(d);
 
 var address = "Coimbatore";
 console.log(address);
+
+console.log("end")
+
+*/
+ 
+
+// ! Example 2
+
+console.log("start")
+
+var a = 10;
+console.log(a);
+
+let b = 20;
+console.log(b);
+
+const c = 30;
+console.log(c);
 
 console.log("end")
