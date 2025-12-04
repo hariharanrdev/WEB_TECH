6bf9e071ace0945d4afb4ef6893dfd18d0a8aa1b
hariharanrdev <hariharanrdev@gmail.com>
2025-12-04 12:00:20 +0530

    fix: update Flex.css to adjust justify-content and align-items properties; modify htmlJS.html to comment out math.js script

---
+++ JavaScript/loopingstatements.js
@@ -0,0 +1,22 @@
+console.log("start")
+
+var n = 1;
+while(n<=5) {
+    console.log("N Value is :",n);
+    n++;
+}
+
+console.log("end");
+
+
+console.log("start");
+console.log("arey yaar");
+alert("hareye");
+
+console.log("end");
+
+for (let i = 1; i <= 5; i += 2)
+    {
+
+
+    } 
