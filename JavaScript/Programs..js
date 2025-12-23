6cc023a9c1854054a2e03c269f63557c9c15d71b
hariharanrdev <hariharanrdev@gmail.com>
2025-12-23 13:15:59 +0530

    refactor: clean up Programs..js and add tempCodeRunnerFile.js for number programs

--- JavaScript/Programs..js
+++ JavaScript/Programs..js
@@ -1,4 +1,5 @@
 //! Programs of numbers
+
 //? wap to print numbers from 1 to 10
 // case-1
 let i = 1;
@@ -6,6 +7,7 @@ while (i <= 10) {
   console.log(i);
   i++;
 }
+
 // case-2
 for (let i = 1; i <= 10; i++) {
   console.log(i);
@@ -16,5 +18,12 @@ for (let i = 1; i <= 10; i++) {
 for(let i=0;i<=10;i=i+2){
     console.log(i);
 }
+
+
+
+
+
+
+
 //! Programs of strings  
 //! Programs of Arrays
---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+//! Programs of numbers
+// //? wap to print numbers from 1 to 10
+// // case-1
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// // case-2
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
