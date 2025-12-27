d855dfd69e014b468b4c6cca2f60fda56aded761
hariharanrdev <hariharanrdev@gmail.com>
2025-12-27 11:16:30 +0530

    refactor: improve comments and formatting in Programs..js for clarity

--- JavaScript/Programs..js
+++ JavaScript/Programs..js
@@ -1,26 +1,38 @@
 //! Programs of numbers
 
 //? wap to print numbers from 1 to 10
-// case-1
+//! case-1
 let i = 1;
 while (i <= 10) {
   console.log(i);
   i++;
 }
 
-// case-2
+//! case-2
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
 
-// wap to print even numbers from 1 to 10
-// case-1
+// ?wap to print even numbers from 1 to 10
+//! case-1
 for(let i=0;i<=10;i=i+2){
-    console.log(i);
 }
+console.log(i);
 
+//?wap to print sum of odd numbers from  1 to 10?
+//! case-1
+let sum=0;
+for(let i=1;i<=10;i=i+2){
+  sum=sum+i;
+}
+console.log("sum of odd numbers:",sum);
 
-
+//?wap to print mulltiplication of numbers from 1 to 5?
+let mul=1;
+for(let i=1;i<=5;i++){
+  mul=mul*i
+}
+console.log(mul);
 
 
 
