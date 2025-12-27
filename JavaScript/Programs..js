dc32beaffa5413e2ddcf0a2201284bd60bd6ce5c
hariharanrdev <hariharanrdev@gmail.com>
2025-12-27 12:28:23 +0530

    refactor: reorganize and comment string programs in Programs..js for clarity

--- JavaScript/Programs..js
+++ JavaScript/Programs..js
@@ -1,3 +1,4 @@
+/*
 //! Programs of numbers
 
 //? wap to print numbers from 1 to 10
@@ -33,9 +34,33 @@ for(let i=1;i<=5;i++){
   mul=mul*i
 }
 console.log(mul);
+*/
 
 
 
+//! Programs of strings
 
-//! Programs of strings  
+//? palindrome or reverse a string
+let s = "javascript";
+let res = s.split("").reverse().join("");
+console.log(res);  
+
+//? palindrome or reverse a string using array methods
+
+let s = "javascript";
+let ar = s.split("");
+console.log(ar);
+
+ar = ar.reverse();
+console.log(ar);
+
+let rev = ar.join("");
+console.log(rev);
+
+if (s == rev) {
+  console.log("palindrome");
+} else {
+  console.log("not palindrome");
+}
+
 //! Programs of Arrays
