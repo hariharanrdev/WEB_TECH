44e2beec7772b6a2f3d6f4bae19c5c8b10ffe4a0
hariharanrdev <hariharanrdev@gmail.com>
2025-12-30 12:56:32 +0530

    refactor: update number and string programs in Programs..js; add array max/min functions and clean up comments

--- JavaScript/Programs..js
+++ JavaScript/Programs..js
@@ -34,14 +34,12 @@ for(let i=1;i<=5;i++){
   mul=mul*i
 }
 console.log(mul);
-*/
 
 
 
 //! Programs of strings
 
 //? palindrome or reverse a string - comprehensive way!.
-/*
 let s = "javascript";
 let res = s.split("").reverse().join("");
 console.log(res);  
@@ -63,7 +61,6 @@ if (s == rev) {
 } else {
   console.log("not palindrome");
 }
-*/
 
 let s = "I love you chellam";
 let res = "";
@@ -85,4 +82,65 @@ console.log(res);
 let s = "I love you chellam";
 let res = "";
 
+
+//? PRINT FINAL STRING AFTER SKIPPING REPEATED CHARACTERS.
+let s = "hariharan";
+let res = "";
+for(let char of s){
+  if (res.indexOf(char) === -1){
+      res = res + char;
+  }
+}
+console.log(res);
+*/
+
 //! Programs of Arrays
+
+//?maximum number in a  array
+// input:[]
+
+let ar = [10, 20, 5, 40, 15, 25, 60, 30, 80, 70, 90];
+
+let max = 0;
+for (let num of ar){
+  if (num > max){
+    max = num;
+  }
+}
+console.log("maximum number:", max);
+
+//? minimum number in a array
+let ar1 = [10, 20, 5, 40, 15, 25, 60, 30, 80, 70, 90];
+let min = ar1[0];
+for (let num of ar1){
+  if (num < min){
+    min = num;
+  }
+}
+console.log("minimum number:", min);
+
+//?marks greater than or equal to 35
+let marks = [10, 45, 78, 32, 89, 90, 23, 67, 54, 12, 38];
+res=[];
+for (let mark of marks){
+  if (mark >= 35){
+    res.push(mark);
+  }
+}
+console.log("passed marks:",res);
+
+
+
+//?remove duplicates from an array
+let ar2 = [10, 20, 5, 40, 15, 25, 60, 30, 80, 70, 90, 10, 20, 5, 40];
+let unique = [];
+for (let num of ar2)
+{
+  if (unique.indexOf(num) === -1){
+    unique.push(num);
+  }
+}
+console.log("unique array:", unique);
+
+
+//! Programs of Objects
